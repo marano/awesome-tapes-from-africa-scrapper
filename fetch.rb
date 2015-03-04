93da50414592d8eb0055cdc584bf0715a18bfd81
marano <thiagomarano@gmail.com>
2015-03-04 12:54:11 -0300

    Get album data via url directly instead of opening it on chrome.

--- fetch.rb
+++ fetch.rb
@@ -1,9 +1,11 @@
 require 'json'
 require 'fileutils'
+require 'open-uri'
 require 'rubygems'
 require 'capybara'
 require 'capybara/dsl'
 require 'pry'
+require 'nokogiri'
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
@@ -15,8 +17,7 @@ Capybara.app_host = 'http://www.awesometapes.com/'
 
 include Capybara::DSL
 
-visit('/')
-categories = [
+@categories = [
   {title: '00s', href: '/category/00s/'},
   {title: '70s', href: '/category/70s/'},
   {title: '80s', href: '/category/80s/'},
@@ -28,45 +29,47 @@ categories = [
   {title: 'Southern Africa', href: '/category/southern-africa/'},
   {title: 'Uncategorized', href: '/category/uncategorized/'},
   {title: 'West Africa', href: '/category/west-africa/'}
-][0..1]
+]
 
-links = page.find('#content_inside').all('.post_box a').map { |link| link['href'] }
+SOUND_CLOUD_ADDRESS = 'http://w.soundcloud.com/player/'
 
-sound_cloud_address = 'http://w.soundcloud.com/player/'
+@albums_data = []
 
-data = []
-data = categories.map do |category|
-  visit(category[:href])
+def fetch_album_data_via_link(album_link, category)
+  puts "Fetching album: #{album_link}"
+  existing_album_data = @albums_data.find { |album_data| album_data[:href] == album_link }
 
-  should_keep_scrolling = true
-  while should_keep_scrolling
-    page.execute_script('window.scrollTo(0, 1000000000)')
-    is_spinning = page.has_css?('#infscr-loading', visible: true)
-    if !is_spinning
-      should_keep_scrolling = false
-    end
-    while page.has_css?('#infscr-loading', visible: true)
-      sleep 0.5
-    end
-  end
-
-  albums = []
-
-  links[0..1].each do |link|
-    visit(link)
+  if existing_album_data
+    puts "Album already in categories: #{existing_album_data[:categories]}"
+    existing_album_data[:categories] << category[:title]
+    puts "Album now in categories: #{existing_album_data[:categories]}"
+  else
+    doc = Nokogiri::HTML(open(album_link), nil, 'UTF-8')
   
-    is_sound_cloud = page.all('iframe').any? do |iframe|
-      iframe['src'].include?(sound_cloud_address)
+    is_sound_cloud = doc.css('iframe').any? do |iframe|
+      iframe['src'].include?(SOUND_CLOUD_ADDRESS)
     end
+
+    puts "is sound cloud: #{is_sound_cloud}"
   
     if !is_sound_cloud
-      content = page.find('.single_left')
-  
-      header = content.find('h1').text
+      content = doc.at_css('.single_left')
+
+      if content.at_css('#jp-relatedposts')
+        content.at_css('#jp-relatedposts').remove
+      end
+      if content.at_css('#comments')
+        content.at_css('#comments').remove
+        content.at_css('.commentlist').remove
+      end
+      content.at_css('#respond').remove
   
       details = {
-        category: category[:title]
+        href: album_link,
+        categories: [category[:title]]
       }
+
+      header = content.at_css('h1').content
   
       if header.include?('—')
         album_data = header.split('—')
@@ -75,26 +78,71 @@ data = categories.map do |category|
       else
         details[:title] = header.strip
       end
-  
-      details[:tracks] = content.all('a').map do |link|
-        {
-          title: link.text.strip,
+
+      details[:tracks] = []
+
+      content.css('a').select { |link| link['href'].end_with?('.mp3') }.each do |link|
+        details[:tracks] << {
+          title: link.content.strip,
           href: link['href']
         }
       end
-  
-      albums << details
-    end
-    
-  end
 
-  {
-    category: category[:title],
-    albums: albums
-  }
+      puts "Tracks fetched: #{details[:tracks].size}"
+
+      details[:covers] = content.css('img').map do |image|
+        {
+          title: image['alt'].strip,
+          src: image['src'].split('?')[0]
+        }
+      end
+
+      puts "Covers fetched: #{details[:covers].size}"
+
+      @albums_data << details
+      puts ""
+    end
+  end
+  puts ""
 end
 
-FileUtils.mkdir_p('output')
-output_file = 'output/data.json'
-FileUtils.rm_rf(output_file)
-File.open(output_file, 'w') { |file| file.write(JSON.generate(data)) }
+def fetch_all_categories
+  @categories.each do |category|
+    puts "Fetching category (#{category[:title]}) links..."
+    visit(category[:href])
+
+    should_keep_scrolling = true
+    while should_keep_scrolling
+      puts "Scrolling..."
+      page.execute_script('window.scrollTo(0, 1000000000)')
+      is_spinning = page.has_css?('#infscr-loading', visible: true)
+      if !is_spinning
+        should_keep_scrolling = false
+      end
+      while page.has_css?('#infscr-loading', visible: true)
+        sleep 0.5
+      end
+    end
+
+    puts ""
+
+    albums_links = page.all('.post_box a').map { |link| link['href'] }
+    puts "Links fetched: #{albums_links.size}."
+    puts ""
+
+    albums_links.each do |album_link|
+      fetch_album_data_via_link(album_link, category)
+    end
+    puts ""
+  end
+end
+
+def save_data_to_file
+  FileUtils.mkdir_p('output')
+  output_file = 'output/data.json'
+  FileUtils.rm_rf(output_file)
+  File.open(output_file, 'w') { |file| file.write(JSON.generate(@albums_data)) }
+end
+
+fetch_all_categories
+save_data_to_file
