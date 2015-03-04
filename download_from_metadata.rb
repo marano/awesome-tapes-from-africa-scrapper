1b189b32ebac82807c7df3f4788953190de25ec5
marano <thiagomarano@gmail.com>
2015-03-04 19:52:01 -0300

    Skip track file if it is already downloaded. Indicate progress.

--- download_from_metadata.rb
+++ download_from_metadata.rb
@@ -9,47 +9,57 @@ data = JSON.parse(File.read('output/data.json'))
 
 def save(from_url, target_file)
   FileUtils.mkdir_p(File.dirname(target_file))
-  File.open(target_file, 'w') do |save_file|
-    open(from_url, 'rb') do |read_file|
+  open(from_url, 'rb') do |read_file|
+    File.open(target_file, 'w') do |save_file|
       save_file.write(read_file.read)
     end
   end
 end
 
+current_album = 0
 data.each do |album|
-  puts "Downloading album #{album['artist']} - #{album['title']} ..."
+  current_album = current_album + 1
+  puts "Downloading album #{album['artist']} - #{album['title']} [#{current_album}/#{data.size}] ..."
 
   album_folder = "output/downloaded/#{album['artist'] || album['title']}/#{album['title']}"
 
   cover_local_path = "#{album_folder}/cover.jpg"
   save(album['covers'].first['src'], cover_local_path)
 
-  album['tracks'][0..0].each do |track|
-    puts "Downloading track #{track['title']} ..."
+  current_track = 0
+
+  album['tracks'].each do |track|
+    current_track = current_track + 1
+
+    puts "Downloading track #{track['title']} [#{current_track}/#{album['tracks'].size}] ..."
 
     local_path = "#{album_folder}/#{track['title']}.mp3"
-    save(track['href'], local_path)
-    Mp3Info.open(local_path) do |mp3|
-      if mp3.tag.title.nil? || mp3.tag.title == ''
-        puts "Title tag missing!"
-        mp3.tag.title = track['title']
-      elsif mp3.tag.title != track['title']
-        puts "Title is different from metadata! '#{mp3.tag.title}' != '#{track['title']}'"
-      end
-      if mp3.tag.artist.nil? || mp3.tag.artist == ''
-        puts "Artist tag missing!"
-        mp3.tag.artist = album['artist']
-      elsif mp3.tag.artist != album['artist']
-        puts "Artist is different from metadata! '#{mp3.tag.artist}' != '#{album['artist']}'"
-      end
-      if mp3.tag.album.nil? || mp3.tag.album == ''
-        puts "Album tag missing!"
-        mp3.tag.album = album['title']
-      elsif mp3.tag.album != album['title']
-        puts "Album is different from metadata! '#{mp3.tag.album}' != '#{album['title']}'"
-      end
+    if File.exists?(local_path)
+      puts "File already exists, skipping!"
+    else
+      save(track['href'], local_path)
+      Mp3Info.open(local_path) do |mp3|
+        if mp3.tag.title.nil? || mp3.tag.title == ''
+          puts "Title tag missing!"
+          mp3.tag.title = track['title']
+        elsif mp3.tag.title != track['title']
+          puts "Title is different from metadata! '#{mp3.tag.title}' != '#{track['title']}'"
+        end
+        if mp3.tag.artist.nil? || mp3.tag.artist == ''
+          puts "Artist tag missing!"
+          mp3.tag.artist = album['artist']
+        elsif mp3.tag.artist != album['artist']
+          puts "Artist is different from metadata! '#{mp3.tag.artist}' != '#{album['artist']}'"
+        end
+        if mp3.tag.album.nil? || mp3.tag.album == ''
+          puts "Album tag missing!"
+          mp3.tag.album = album['title']
+        elsif mp3.tag.album != album['title']
+          puts "Album is different from metadata! '#{mp3.tag.album}' != '#{album['title']}'"
+        end
 
-      mp3.tag2.add_picture(File.new(cover_local_path, 'rb').read)
+        mp3.tag2.add_picture(File.new(cover_local_path, 'rb').read)
+      end
     end
 
     puts ''
